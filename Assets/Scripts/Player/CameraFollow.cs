82d0ef3151417a5609de84ee60c30462fe17e59a
Jesse <jm.krijgsman@Student.avans.nl>
2022-05-01 17:07:00 +0200

    Updated 2D -> 3D switch

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -10,11 +10,17 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private float smoothSpeed = 0.125f;
     [SerializeField] public Vector3 offset;
 
+    [SerializeField] public bool hasPerspective;
+    
     private void FixedUpdate()
     {
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.LookAt(target);
+
+        if (hasPerspective)
+            transform.LookAt(target);  
+        
+        
         transform.position = smoothedPosition;
         
         
