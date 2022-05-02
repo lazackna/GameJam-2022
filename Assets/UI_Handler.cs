53e436baed7c57385b327ba31c03af51da8991f9
Jesse <jm.krijgsman@Student.avans.nl>
2022-05-02 21:27:52 +0200

    Added key indicator 2

--- Assets/UI_Handler.cs
+++ Assets/UI_Handler.cs
@@ -9,16 +9,20 @@ public class UI_Handler : MonoBehaviour
 
     private static GameObject DS_Image;
     private static Image DS_Loader;
+    private static GameObject DS_Key;
 
     private static GameObject N64_Image;
     private static Image N64_Loader;
+    private static GameObject N64_Key;
 
     private void Start()
     {
         DS_Image = GameObject.Find("Canvas/DS_Loader/DS_image");
+        DS_Key = GameObject.Find("Canvas/Help-Q");
         DS_Loader = GameObject.Find("Canvas/DS_Loader").GetComponent<Image>();
         
         N64_Image = GameObject.Find("Canvas/N64_Loader/N64_Image");
+        N64_Key = GameObject.Find("Canvas/Help-E");
         N64_Loader = GameObject.Find("Canvas/N64_Loader").GetComponent<Image>();
         
         DS_Image.SetActive(false);
@@ -31,6 +35,7 @@ public class UI_Handler : MonoBehaviour
     {
         DS_Image.SetActive(show);
         DS_Loader.gameObject.SetActive(show);
+        DS_Key.SetActive(show);
         
         if (show)
             DS_Loader.fillAmount = percent / 100f;
@@ -40,6 +45,7 @@ public class UI_Handler : MonoBehaviour
     {
         N64_Image.SetActive(show);
         N64_Loader.gameObject.SetActive(show);
+        N64_Key.SetActive(show);
         
         if (show)
             N64_Loader.fillAmount = percent / 100f;
