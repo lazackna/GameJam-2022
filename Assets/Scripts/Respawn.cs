f71f30c8df46b8b4f08a14f98b44d12927dedf29
jasper baijens <jasperbayens@outlook.com>
2022-05-03 15:58:11 +0200

    Added death sound

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -8,10 +8,13 @@ public class Respawn : MonoBehaviour
 {
 
     [SerializeField] private GameObject gameoverCanvas;
+
+    private AudioManager audioManager;
     // Start is called before the first frame update
     void Start()
     {
         gameoverCanvas.SetActive(false);
+        audioManager = GetComponent<AudioManager>();
     }
 
     // Update is called once per frame
@@ -33,6 +36,8 @@ public class Respawn : MonoBehaviour
     {
         //show gameover screen.
         gameoverCanvas.SetActive(true);
+        if(audioManager != null)
+            audioManager.Play("death");
         yield return new WaitForSeconds(4);
         //Reload the entire scene.
         SceneManager.LoadScene(0);
